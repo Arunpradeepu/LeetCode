e86908e18d316b518f9585dc0d980cf890a28686
Arunpradeepu <154908628+Arunpradeepu@users.noreply.github.com>
2025-07-29 22:58:09 +0530

    Add files via upload

---
+++ rotateString.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if(s.equals(goal)){
+            return true;
+        }
+        for(int i = 1;i<s.length();i++){
+            String rot = s.substring(i) + s.substring(0,i);
+            if(rot.equals(goal)){
+                return true;
+            } 
+        }
+        return false;
+    }
+}
