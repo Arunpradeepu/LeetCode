48dec57ee8a9394d0dad2000e37338278caaca49
Arunpradeepu <154908628+Arunpradeepu@users.noreply.github.com>
2025-12-14 16:29:52 +0530

    Add files via upload

---
+++ isUgly.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isUgly(int n) {
+        int num = n;
+        while(num > 1) {
+			if(num % 2 == 0) {
+				num /= 2;
+			}
+			else if(num % 3 == 0) {
+				num /= 3;
+			}
+			else if(num % 5 == 0) {
+				num /= 5;
+			}
+
+            else{
+                return false;
+            }
+		}
+		if(num == 1) {
+			return true;
+		}
+		return false;
+	}
+    
+}
