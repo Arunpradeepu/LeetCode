d6a6c470810e8b68f57000328d6444d4b691feb9
Arunpradeepu <154908628+Arunpradeepu@users.noreply.github.com>
2026-02-08 14:07:54 +0530

    Add files via upload

---
+++ minSubArrayLen.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int i = 0;
+        int ans = Integer.MAX_VALUE;
+        int sum = 0;
+
+        for(int j = 0;j<nums.length;j++){
+            sum += nums[j];
+
+            while(sum >= target){
+                ans = Math.min(ans,j-i+1);
+                sum -= nums[i];
+                i++;
+            }
+
+
+
+        }
+
+        if(ans == Integer.MAX_VALUE){
+            ans = 0;
+        }
+        return ans;
+    }
+}
