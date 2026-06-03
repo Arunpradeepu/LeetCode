6c72cae55402770a658ecffacbe804b672b42a3e
Arunpradeepu <154908628+Arunpradeepu@users.noreply.github.com>
2026-06-03 18:17:12 +0530

    Add files via upload

---
+++ maxPathSum.java
@@ -0,0 +1,26 @@
+class Solution {
+    
+    public int maxPathSum(TreeNode root) {
+        int[] max = new int[1];
+        max[0] = Integer.MIN_VALUE;
+
+        helper(root,max);
+
+        return max[0];
+    }
+    public static int helper(TreeNode root,int max[]){
+        if(root == null){
+            return 0;
+        }
+
+        int lh = Math.max(0,helper(root.left,max));
+        int rh = Math.max(0,helper(root.right,max));
+
+        
+
+        max[0] = Math.max( max[0],root .val + lh + rh);
+
+        return root.val + Math.max(lh,rh);
+
+    }
+}
