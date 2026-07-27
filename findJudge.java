0d0b26ae7cf638208cd5a816d521f7f77eecc31a
Arunpradeepu <154908628+Arunpradeepu@users.noreply.github.com>
2026-07-27 13:05:53 +0530

    Add files via upload

---
+++ findJudge.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+
+        if(trust.length == 0 && n == 1){
+            return 1;
+        }
+
+        int arr[] = new int[n+1];
+
+        for(int i=0;i<trust.length;i++){
+            arr[trust[i][0]]--;
+            arr[trust[i][1]]++;
+        }
+
+        for(int i=0;i<arr.length;i++){
+            int val = arr[i];
+            if(val ==  n - 1){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
